f1a20d5b9c5e472db3b8f39c064fdef5be03bcd7
adityagupta0251 <itsokkadi0251@gmail.com>
2025-04-16 17:41:58 +0530

    fixed complete homepagelayout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+module.exports = {
+  // If you extend Next.js presets, you might have:
+  extends: [
+    "next",
+    "next/core-web-vitals"
+  ],
+  rules: {
+    // Disable unescaped entity warnings in JSX
+    "react/no-unescaped-entities": "off",
+    // Disable warning for using <img> elements in Next.js
+    "@next/next/no-img-element": "off"
+  }
+};
